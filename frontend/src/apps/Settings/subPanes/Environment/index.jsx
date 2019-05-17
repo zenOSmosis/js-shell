f6d3da605cada256dabd0b219db595f20e0b48a0
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-05-17 17:53:43 -0400

    Prototyping preliminary Environment subPane

---
+++ frontend/src/apps/Settings/subPanes/Environment/index.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import socketQuery, { socketAPIRoutes } from 'utils/socketQuery';
+
+export default class Environment extends Component {
+  state = {
+    remoteEnv: {}
+  };
+
+  componentDidMount() {
+    this.fetchRemoteEnv();
+  }
+
+  async fetchRemoteEnv() {
+    try {
+      const remoteEnv = await socketQuery(socketAPIRoutes.SOCKET_API_ROUTE_ENV);
+
+      this.setState({
+        remoteEnv
+      });
+    } catch (exc) {
+      throw exc;
+    }
+  }
+
+  render() {
+    const { remoteEnv } = this.state;
+
+    return (
+      <div style={{ textAlign: 'left' }}>
+        <div>
+          <div>Local</div>
+          {
+            Object.keys(process.env).map((envKey, idx) => {
+              return (
+                <div key={idx}>
+                  {envKey}: {process.env[envKey]}
+                </div>
+              )
+            })
+          }
+        </div>
+
+        <div>
+          <div>Remote</div>
+          {
+            Object.keys(remoteEnv).map((envKey, idx) => {
+              return (
+                <div key={idx}>
+                  {envKey}: {remoteEnv[envKey]}
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
