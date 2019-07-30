65c2026d85ff5ec3047cfd021aa6190926105817
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-07-30 07:32:54 -0600

    Commented-out auto echo & noise suppression

--- frontend/src/process/MicrophoneProcess.js
+++ frontend/src/process/MicrophoneProcess.js
@@ -35,14 +35,20 @@ class MicrophoneProcess extends ClientAudioProcess {
   async _startMic() {
     try {
       // TODO: Make these options confiurable via this._options
-      // Borrowed from screen capture tutorial (even though this class is fully audio)
-      // @see https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
       const mediaOptions = {
+        /*
+        // Borrowed from screen capture tutorial (even though this class is fully audio)
+        // @see https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
+        // Notes: When using STT (Homayoon), it seems (at least on of) these
+        // options adversely affect the speech recognition
         audio: {
           echoCancellation: true,
           noiseSuppression: true,
           sampleRate: 44100
         }
+        */
+       // or
+       audio: true
       };
 
       this._outputStream = await navigator.mediaDevices.getUserMedia(mediaOptions);
