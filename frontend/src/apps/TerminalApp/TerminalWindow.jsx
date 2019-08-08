f0c4f71136170dd11135f9f24df228261f046ed3
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-08-07 23:09:10 -0600

    Prototyping Terminal data passing (still buggy)

--- frontend/src/apps/TerminalApp/TerminalWindow.jsx
+++ frontend/src/apps/TerminalApp/TerminalWindow.jsx
@@ -8,6 +8,44 @@ export default class TerminalWindow extends Component {
   componentDidMount() {
     connectTerminal(this._xterm)
   }
+
+  componentWillUnmount() {
+    console.warn('TODO: Handle forcing of terminal to disconnect');
+  }
+
+  _connectTerminal() {
+    // TODO: Fix this
+    // No idea what this does
+    // term.winptyCompatInit();
+
+    /*
+    // Open the websocket connection to the backend
+    const protocol = (window.location.protocol === 'https:') ? 'wss://' : 'ws://';
+    const port = ':3002';//window.location.port ? `:${window.location.port}` : '';
+    const socketUrl = `${protocol}${window.location.hostname}${port}/shell`;
+    console.debug('connecting to:', socketUrl)
+    const socket = new WebSocket(socketUrl);
+    
+    // Attach the socket to the terminal
+    socket.onopen = (ev) => { 
+      console.debug('socket connected')
+      term.attach(socket); 
+    }
+  
+    socket.onerror = (ev) => { 
+      console.debug('error', ev)
+    }
+    */
+
+    this._socketChannel.on('data', (data) => {
+      this._xterm.write(this._socketChannel.ab2str(data));
+    });
+  }
+
+  _handleInput = (data) => {
+    this._socketChannel.write(this._socketChannel.str2ab(data));
+  };
+
   render() {
     const { ...propsRest } = this.props;
     return (
@@ -30,26 +68,4 @@ export default class TerminalWindow extends Component {
   }
 
   
-}
-
-function connectTerminal(xterm) {
-  const term = xterm.getTerminal();
-  // No idea what this does
-  term.winptyCompatInit();
-  // Open the websocket connection to the backend
-  const protocol = (window.location.protocol === 'https:') ? 'wss://' : 'ws://';
-  const port = ':3002';//window.location.port ? `:${window.location.port}` : '';
-  const socketUrl = `${protocol}${window.location.hostname}${port}/shell`;
-  console.log('connecting to:', socketUrl)
-  const socket = new WebSocket(socketUrl);
-  // Attach the socket to the terminal
-  socket.onopen = (ev) => { 
-    console.log('socket connected')
-    term.attach(socket); 
-  }
-
-  socket.onerror = (ev) => { 
-    console.log('error', ev)
-  }
-  // Not going to worry about close/error for the websocket
 }
