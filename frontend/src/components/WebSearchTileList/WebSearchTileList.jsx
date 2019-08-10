30e24170593299c2768d3141c0559e4cade36a45
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-08-10 14:17:07 -0600

    Implemented initial video playing from search results

--- frontend/src/components/WebSearchTileList/WebSearchTileList.jsx
+++ frontend/src/components/WebSearchTileList/WebSearchTileList.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import Full from '../Full';
 import TileList, { Tile } from '../TileList';
 import socketAPIQuery from 'utils/socketAPI/socketAPIQuery';
 import { SOCKET_API_ROUTE_WEB_SEARCH } from 'shared/socketAPI/socketAPIRoutes';
+import { Spin, Icon } from 'antd';
+import Center from 'components/Center';
+import Cover from 'components/Cover';
+
+const AntIcon = <Icon type="loading" style={{ fontSize: 68 }} spin />;
 
 /**
  * A wrapper for TileList with web search results.
@@ -13,13 +19,18 @@ class WebSearchTileList extends Component {
     super(props);
 
     this.state = {
-      results: []
+      results: [],
+      isSearching: false
     };
   }
 
   async query(value) {
     try {
       try {
+        this.setState({
+          isSearching: true
+        });
+
         // TODO: Allow this to be cancellable
         const results = await socketAPIQuery(SOCKET_API_ROUTE_WEB_SEARCH, {
           query: value
@@ -27,7 +38,8 @@ class WebSearchTileList extends Component {
 
         // TODO: Don't set if the component has already unmounted
         this.setState({
-          results
+          results,
+          isSearching: false
         });
       } catch (exc) {
         throw exc;
@@ -37,41 +49,84 @@ class WebSearchTileList extends Component {
     }
   }
 
+  /*
+  componentDidUpdate(prevProps, prevState) {
+    console.debug({
+      prevProps,
+      prevState
+    });
+  }
+  */
+
+  /**
+   * @param{WebSearchQueryResult} result
+   */
+  _handleResultSelect = (result) => {
+    const { onResultSelect } = this.props;
+    if (typeof onResultSelect === 'function') {
+      onResultSelect(result);
+    }
+  };
+
   render() {
     const {
       query,
       ...propsRest
     } = this.props;
 
-    const { results } = this.state;
+    const { results, isSearching } = this.state;
 
     return (
-      <TileList
-        {...propsRest}
-      >
+      <Full>
+        <TileList
+          {...propsRest}
+        >
+          {
+            results.map((result, idx) => {
+              // WebSearchQueryResult
+              const {
+                url,
+                thumbnail,
+                title,
+                content,
+                template,
+                publishedDate
+              } = result;
+
+              return (
+                <Tile
+                  key={idx}
+                  title={title}
+                  onClick={evt => { this._handleResultSelect(result) }}
+                >
+                  <img src={thumbnail} width="100%" />
+                </Tile>
+              );
+            })
+          }
+        </TileList>
+
         {
-          results.map((result, idx) => {
-            // WebSearchQueryResult
-            const {
-              url,
-              thumbnail,
-              title,
-              content,
-              template,
-              publishedDate
-            } = result;
-            
-            return (
-              <Tile
-                key={idx}
-                title={title}
-              >
-                <img src={thumbnail} width="100%" />
-              </Tile>
-            );
-          })
+          isSearching &&
+          <Cover>
+            <Center>
+              <div>
+                <div style={{ margin: 20 }}>
+                  Searching...
+                    </div>
+
+                <div>
+                  <Spin size="large" indicator={AntIcon} />
+                </div>
+
+                <div style={{ margin: 20 }}>
+                  <button>Cancel</button>
+                </div>
+              </div>
+            </Center>
+          </Cover>
         }
-      </TileList>
+      </Full>
     );
   }
 }
