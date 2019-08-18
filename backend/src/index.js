9680d6b185f24d39b65ee5b2063542d5d00a0da6
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-08-18 13:54:21 -0600

    Fixed module-alias implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,7 @@
+// Enable module aliases
+// @see https://www.npmjs.com/package/module-alias
+require('module-alias/register');
+
 // "esm is the world’s most advanced ECMAScript module loader."
 // @see https://www.npmjs.com/package/esm
 require('esm');
