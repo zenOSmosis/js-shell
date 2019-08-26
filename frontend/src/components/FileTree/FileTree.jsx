66f3e057ffe8716a2d39febb15e19645b39e8587
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-08-26 12:45:08 -0600

    Implemented preliminary FileTree utils

--- frontend/src/components/FileTree/FileTree.jsx
+++ frontend/src/components/FileTree/FileTree.jsx
@@ -4,14 +4,16 @@
 
 import React, { Component } from 'react';
 import FileTreeNodeComponent from './FileTreeNode';
+import {
+  absorb,
+  fetchDirTreeData,
+  findFileTreeNodeWithPath
+} from './FileTree.utils';
 import Scrollable from '../Scrollable';
 import { Treebeard } from 'react-treebeard';
 // import decorators from 'react-treebeard/lib/components/decorators';
-import { dirDetail } from 'utils/socketFS';
 import './FileTree.typedefs';
 import PropTypes from 'prop-types';
-
-const _absorb = () => null;
 
 const decorators = {
   Loading: (props) => {
@@ -95,7 +97,7 @@ class FileTree extends Component {
   async componentDidMount() {
     try {
       // TODO: Fetch root path / directory separator (don't hardcode)
-      const treeData = await this.fetchDirTreeData('/');
+      const treeData = await fetchDirTreeData('/');
 
       this.setState({
         treeData
@@ -103,133 +105,6 @@ class FileTree extends Component {
     } catch (exc) {
       throw exc;
     }
-  }
-
-  async fetchDirTreeData(path) {
-    try {
-      const rawDirDetail = await dirDetail(path);
-
-      const branchData = {
-        isDir: rawDirDetail.isDir,
-        name: rawDirDetail.base,
-        path: rawDirDetail.path, // Normalized path
-        toggled: true,
-        children: (() => {
-          if (!rawDirDetail.children) {
-            return undefined;
-          }
-
-          return rawDirDetail.children.filter(child => {
-            // Skip error nodes
-            if (child.error) {
-              console.warn('Skipping errored child', child);
-            }
-            return !child.error
-          }).map(child => {
-            const { base: childBase, isDir, ...childRest } = child;
-
-            return {
-              // Sub-children don't need to render values at this branch level,
-              // and setting undefined as the value removes the dropdown toggle
-              children: (isDir ? [] : undefined),
-              
-              ...childRest,
-              isDir,
-              name: childBase,
-              toggled: false
-            }
-          });
-        })()
-      };
-
-      return branchData;
-    } catch (exc) {
-      throw exc;
-    }
-  }
-
-  /**
-   * @param {string} path
-   * @return {FileTreeNodeWithWalkPath} 
-   */
-  findFileTreeNodeWithPath(path) {
-    const { treeData } = this.state;
-
-    // Absorb fileTreeNode data to mute compiler warnings with eval'd code
-    _absorb(treeData);
-
-    const SEARCH_KEY_PATH = 'path';
-    const SEARCH_KEY_CHILDREN = 'children';
-
-    // Recursively walks the path in order to find the searched tree node
-    const r_find = (path, walkPath = '') => {
-      // eslint-disable-next-line 
-      const subKeys = Object.keys(eval(`treeData${walkPath}`));
-      for (const key of subKeys) {
-        switch (key) {
-          case SEARCH_KEY_PATH:
-            // eslint-disable-next-line 
-            const treeDataPath = eval(`treeData${walkPath}[key]`);
-
-            if (path === treeDataPath) {
-              return walkPath;
-            }
-            break;
-
-          case SEARCH_KEY_CHILDREN:
-            // eslint-disable-next-line 
-            const treeDataChildren = eval(`treeData${walkPath}[key]`);
-
-            if (Array.isArray(treeDataChildren)) {
-              const lenTreeDataChildren = treeDataChildren.length;
-
-              for (let i = 0; i < lenTreeDataChildren; i++) {
-                const child = treeDataChildren[i];
-
-                const { path: childPath } = child;
-
-                if (path === childPath) {
-                  // Add the current path to the walkPath
-                  walkPath = `${walkPath}.${SEARCH_KEY_CHILDREN}[${i}]`;
-
-                  return walkPath;
-                } else {
-                  // Determine if the current child path is within our search path
-                  if (path.includes(childPath)) {
-                    // Add the current path to the walkPath
-                    walkPath = `${walkPath}.${SEARCH_KEY_CHILDREN}[${i}]`;
-
-                    return r_find(path, walkPath);
-                  }
-                }
-              }
-            }
-            break;
-
-          default:
-            // Ignore
-            break;
-        }
-      }
-    };
-
-    let walkPath = r_find(path);
-
-    if (!walkPath) {
-      console.warn('Could not obtain walk path for path', path);
-
-      walkPath = '';
-    }
-
-    // if (walkPath) {
-    // eslint-disable-next-line 
-    const fileTreeNode = eval(`this.state.treeData${walkPath}`);
-
-    return {
-      walkPath,
-      fileTreeNode
-    };
-    // }
   }
 
   async handleTreebeardToggle(fileTreeNode) {
@@ -247,7 +122,8 @@ class FileTree extends Component {
 
   async toggleFileTreeNodeWithPath(path) {
     try {
-      let { walkPath, fileTreeNode } = this.findFileTreeNodeWithPath(path);
+      const { treeData } = this.state;
+      let { walkPath, fileTreeNode } = findFileTreeNodeWithPath(treeData, path);
 
       if (!fileTreeNode) {
         console.error('Could not locate fileTreeNode');
@@ -275,7 +151,7 @@ class FileTree extends Component {
     const { treeData } = this.state;
 
     // Absorb fileTreeNode data to mute compiler warnings with eval'd code
-    _absorb(fileTreeNode);
+    absorb(fileTreeNode);
 
     // Graft in the new fileTreeNode data
     // eslint-disable-next-line 
@@ -290,7 +166,7 @@ class FileTree extends Component {
     try {
       const { path } = fileTreeNode;
 
-      fileTreeNode = await this.fetchDirTreeData(path);
+      fileTreeNode = await fetchDirTreeData(path);
       fileTreeNode.toggled = true;
 
       this.graftFileTreeNode(fileTreeNode, walkPath);
