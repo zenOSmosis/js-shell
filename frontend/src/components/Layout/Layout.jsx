ea6baf03aa731f2bf9c274d28580d0b7d7f7448d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-08-28 06:48:13 -0600

    Removed Antd from Layout and cleaned up

---
+++ frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Full from '../Full';
+import { Row, Column } from '../RowColumn';
+import Section from '../Section';
+import style from './Layout.module.css';
+import classNames from 'classnames';
+
+export {
+  Row,
+  Column,
+
+  Section
+};
+
+export const Layout = (props) => {
+  const { className, children, ...propsRest } = props;
+
+  return (
+    <Full className={classNames(style['layout'], className)}>
+      {
+        children
+      }
+    </Full>
+  );
+};
+
+export const Header = (props) => {
+  const { className, children, ...propsRest } = props;
+
+  return (
+    <div
+      {...propsRest}
+      className={classNames(style['header'], className)}
+    >
+      {
+        children
+      }
+    </div>
+  );
+};
+
+export const Content = (props) => {
+  const { className, children, ...propsRest } = props;
+
+  return (
+    <div
+      {...propsRest}
+      className={classNames(style['content'], className)}
+    >
+      {
+        children
+      }
+    </div>
+  );
+};
+export const Footer = (props) => {
+  const { className, children, ...propsRest } = props;
+
+  return (
+    <div
+      {...propsRest}
+      className={classNames(style['footer'], className)}
+    >
+      {
+        children
+      }
+    </div>
+  );
+};
+
+export default Layout;
--- frontend/src/components/Layout/index.jsx
+++ frontend/src/components/Layout/index.jsx
@@ -1,105 +1,24 @@
-// TODO: Create Header, Content, Footer w/ Row/Column, as the handling seems
-// more robust
+import Layout, {
+  Header,
+  Content,
+  Footer,
 
-import React from 'react';
-import { Layout as AntdLayout } from 'antd';
-import { Row, Column } from '../RowColumn';
-import Section from '../Section';
-// import {Layout as AntdLayout, Header as AntdHeader, Sider as AntdSider, Content as AntdContent, Footer as AntdFooter} from 'antd';
-import './style.css';
-const { Header: AntdHeader, Sider: AntdSider, Content: AntdContent, Footer: AntdFooter} = AntdLayout;
-
-export {
+  Section,
+  
   Row,
-  Column,
-
-  Section
-};
-
-const Layout = (props) => {
-  const {className, children, ...propsRest} = props;
-
-  return (
-    <AntdLayout
-      className={`zd-layout core common ${className ? className : ''}`}
-      {...propsRest}
-    >
-      {
-        children
-      }
-    </AntdLayout>
-  );
-};
+  Column
+} from './Layout';
 
 export default Layout;
 export {
-  Layout
-};
+  Layout,
 
-export const Header = (props) => {
-  const {className, children, ...propsRest} = props;
+  Header,
+  Content,
+  Footer,
 
-  return (
-    <AntdHeader
-      className={`zd-layout common ${className ? className : ''}`}
-      {...propsRest}
-    >
-      {
-        children
-      }
-    </AntdHeader>
-  );
-};
+  Section,
 
-export const Sider = (props) => {
-  let {className, width, children, ...propsRest} = props;
-
-  if (typeof width === 'undefined') {
-    width = 100;
-  }
-
-  return (
-    <AntdSider
-      {...propsRest}
-      width={width}
-      className={`zd-layout common ${className ? className : ''}`}
-    >
-      {
-        children
-      }
-    </AntdSider>
-  );
-};
-
-// Sider alias
-export const Aside = Sider;
-
-export const Content = (props) => {
-  const {className, children, ...propsRest} = props;
-
-  return (
-    <AntdContent
-      className={`zd-layout common ${className ? className : ''}`}
-      {...propsRest}
-    >
-      {
-        children
-      }
-    </AntdContent>
-  );
-};
-
-export const Footer = (props) => {
-  const {className, children, ...propsRest} = props;
-
-  return (
-    <AntdFooter
-      className={`zd-layout common ${className ? className : ''}`}
-      {...propsRest}
-    >
-      {
-        children
-      }
-    </AntdFooter>
-  );
+  Row,
+  Column
 };
