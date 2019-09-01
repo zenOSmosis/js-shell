1d3495a74812a767cbb8168a95ff07c59e860216
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-09-01 15:50:16 -0600

    Added Bash exit trap in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+# For a primer on Bash exit traps:
+# @see http://redsymbol.net/articles/bash-exit-traps/
+
+#!/bin/bash
 
 # Executes the Docker Compose configuration with the current user's process & group id
 CURRENT_UID=$(id -u):$(id -g) docker-compose up
+
+function finish {
+  docker-compose down
+}
+
+trap finish EXIT
