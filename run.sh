46c667be8a253e93568f46d217c6066be0a4c6a2
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-09-02 12:00:57 -0600

    Renamed Docker Compose YAML entrypoint to prevent accidental "docker-compose up" generation; re-enabled npm install in dev build; added ROOT_PATH to dev build script

--- run.sh
+++ run.sh
@@ -3,11 +3,16 @@
 
 #!/bin/bash
 
+DOCKER_COMPOSE_FILE='docker-compose.dev.yml';
+echo "Preparing to launch Docker Compose configuration from file ${DOCKER_COMPOSE_FILE}"
+
 # Executes the Docker Compose configuration with the current user's process & group id
-CURRENT_UID=$(id -u):$(id -g) docker-compose up
+CURRENT_UID=$(id -u):$(id -g) docker-compose -f $DOCKER_COMPOSE_FILE up
 
 function finish {
-  docker-compose down
+  echo "Preparing to remove Docker Compose configuration from file ${DOCKER_COMPOSE_FILE}"
+
+  docker-compose -f $DOCKER_COMPOSE_FILE down
 }
 
 trap finish EXIT
