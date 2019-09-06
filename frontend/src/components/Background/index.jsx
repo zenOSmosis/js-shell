2b8be22a262a63dcb09a7fdb814509ab023b2df8
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-09-06 07:41:51 -0600

    Reworked Background to utilize CSS module

--- frontend/src/components/Background/index.jsx
+++ frontend/src/components/Background/index.jsx
@@ -1,28 +1,3 @@
-import React, {Component} from 'react';
-import Cover from '../Cover';
-import Full from '../Full';
-import Image from '../Image';
-import './style.css';
+import Background from './Background';
 
-export default class Background extends Component {
-  render() {
-    let {children, src, className, ...propsRest} = this.props;
-
-    return (
-      <Full
-        {...propsRest}
-        className={`zd-background ${className ? className : ''}`}
-      >
-        <Cover className="zd-background-cover">
-          <Image className="zd-background-image" alt="" src={src} />
-        </Cover>
-        
-        <Cover>
-          {
-            children
-          }
-        </Cover>
-      </Full>
-    );
-  }
-}
+export default Background;
