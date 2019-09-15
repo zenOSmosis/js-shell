f60c31aa4a98aa16a96c45c7942f6423207f41a2
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-09-15 14:46:38 -0600

    Performed cleanup / housekeeping

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -15,15 +15,14 @@ process.env.REACT_APP_SHELL_UI_BUILD_INFO = (() => {
 
 const WorkerPlugin = require('worker-plugin');
 
-// Override create-react-app webpack configs without ejecting
-// @see https://github.com/timarney/react-app-rewired
-
 // @see https://github.com/Microsoft/monaco-editor/issues/82
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 // @see https://dariosky.github.io/2018/08/05/add-proper-hot-reload-to-create-react-app/
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
+// Override create-react-app webpack configs without ejecting
+// @see https://github.com/timarney/react-app-rewired
 module.exports = function override(config, env) {
   if (!config.plugins) {
     config.plugins = [];
@@ -42,32 +41,6 @@ module.exports = function override(config, env) {
     })();
   }
 
-  // config.resolveLoader.modules = ["node_modules", "web_loaders"];
-
-  /*
-  config.module.rules.push({
-    test: /\.worker\.js$/,
-    loader: 'worker-plugin-loader',
-    options: {
-      // name: 'native-worker.[hash].js', // Set a custom name for the output script
-      // inline: false // Inline the worker as a BLOB
-    }
-  });
-  */
-
-  /*
-  // @see https://medium.com/@danilog1905/how-to-use-web-workers-with-react-create-app-and-not-ejecting-in-the-attempt-3718d2a1166b
-  // @see https://www.npmjs.com/package/worker-loader
-  config.module.rules.push({
-    test: /\.worker\.js$/,
-    loader: 'worker-loader',
-    options: {
-      name: 'native-worker.[hash].js', // Set a custom name for the output script
-      inline: false // Inline the worker as a BLOB
-    }
-  });
-  */
-
   // Fix window not defined error in Web Worker
   // @see https://medium.com/@vincentdnl/just-did-all-the-steps-in-the-article-on-a-fresh-cra-install-and-i-get-a-referenceerror-window-is-e200541533d0
   // config.output['globalObject'] = 'this';
@@ -84,15 +57,6 @@ module.exports = function override(config, env) {
   config.plugins.push(
     new MonacoWebpackPlugin()
   );
-
-  /*
-  if (!config.externals) {
-    config.externals = {};
-  };
-  config.externals = {...config.externals, {
-    ...newExternals
-  }};
-  */
 
   // Echo config, for debug purposes
   // Note: This should come last, if echoing complete config
