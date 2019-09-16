4528f90a1f7ed41de2ed029fc692fdcaecd1ca3e
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-09-16 08:22:57 -0600

    Added comments; returned default empty object, if no build info is present

--- frontend/src/utils/buildInfo/getShellUIBuildInfo.js
+++ frontend/src/utils/buildInfo/getShellUIBuildInfo.js
@@ -1,4 +1,18 @@
+/**
+ * Retrieves information about the current Shell build for both frontend &
+ * backend packages.
+ * 
+ * Note: Refer to config-overrides.js for composition.
+ * 
+ * @return {Object}
+ */
 const getShellUIBuildInfo = () => {
+  const data = process.env.REACT_APP_SHELL_UI_BUILD_INFO;
+
+  if (!data) {
+    return {};
+  }
+
   const json = process.env.REACT_APP_SHELL_UI_BUILD_INFO;
 
   return JSON.parse(json);
