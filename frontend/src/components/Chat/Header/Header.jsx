2d213fc55153438e2f5615b105edcca020134604
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-09-29 16:46:48 -0600

    Updated Chat Header to fetch Peer

--- frontend/src/components/Chat/Header/Header.jsx
+++ frontend/src/components/Chat/Header/Header.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Icon } from 'antd';
+import { getPeerWithId } from 'utils/p2p/Peer.class';
 
 class Header extends Component {
   render() {
     const { remotePeerId } = this.props;
 
+    const remotePeer = getPeerWithId(remotePeerId);
+    if (!remotePeer) {
+      return false;
+    }
+
+    const nickname = remotePeer.getNickname();
+
     return (
       <div style={{backgroundColor: 'rgba(255,255,255,.8)', color: '#000', fontSize: '1.4rem', fontWeight: 'bold'}}>
-        {remotePeerId} <Icon type="phone" />
+        {nickname} <Icon type="phone" />
       </div>
     );
   }
