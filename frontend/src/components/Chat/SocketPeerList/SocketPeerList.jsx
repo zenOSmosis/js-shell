fb6a8188482db26b5ec55270f2dec1f750655e64
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-10-06 21:06:13 -0600

    Prototyping SocketPeerList tooltip

--- frontend/src/components/Chat/SocketPeerList/SocketPeerList.jsx
+++ frontend/src/components/Chat/SocketPeerList/SocketPeerList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Row, { Column } from 'components/RowColumn';
-import { Avatar } from 'antd';
+import { Avatar, Tooltip } from 'antd';
 import classNames from 'classnames';
 import styles from './SocketPeerList.module.scss';
 
@@ -32,6 +32,13 @@ class SocketPeerList extends Component {
             const nickname = peer.getNickname();
             const aboutDescription = peer.getAboutDescription();
             const browserOnOs = peer.getBrowserOnOs();
+            const systemInfo = peer.getSystemInfo();
+            const {
+              browser: { name: browserName, version: browserVersion },
+              engine: { name: engineName, version: engineVersion },
+              os: { name: osName, version: osVersion },
+              platform: {type: platformType}
+            } = systemInfo;
 
             return (
               <li
@@ -39,42 +46,81 @@ class SocketPeerList extends Component {
                 title={aboutDescription}
                 className={(Object.is(peer, selectedPeer) ? styles['active'] : null)}
               >
-                <div
-                  onMouseDown={evt => this.selectPeer(peer)}
-                  onTouchStart={evt => this.selectPeer(peer)}
+                <Tooltip
+                  placement="left"
+                  title={
+                    <div className={styles['tooltip']}>
+                      <h1>
+                        {nickname}
+                      </h1>
+                      [ Call Placeholder ]
+
+                      <h2>System Info</h2>
+                      <table style={{width: '100%'}}>
+                        <thead>
+                          <tr>
+                            <td>Kind</td>
+                            <td>Name</td>
+                            <td>Version</td>
+                          </tr>
+                          <tr>
+                            <td>Browser</td>
+                            <td>{browserName}</td>
+                            <td>{browserVersion}</td>
+                          </tr>
+                          <tr>
+                            <td>Engine</td>
+                            <td>{engineName}</td>
+                            <td>{engineVersion}</td>
+                          </tr>
+                          <tr>
+                            <td>OS</td>
+                            <td>{osName}</td>
+                            <td>{osVersion}</td>
+                          </tr>
+                        </thead>
+                      </table>
+                      Platform Type: {platformType}
+                    </div>
+                  }
                 >
-                  <Row>
-                    <Column style={{ textAlign: 'center', minWidth: 42, maxWidth: 42 }}>
-                      <Avatar
-                        size={36}
-                        icon="user"
-                      />
-                    </Column>
+                  <div
+                    onMouseDown={evt => this.selectPeer(peer)}
+                    onTouchStart={evt => this.selectPeer(peer)}
+                  >
+                    <Row>
+                      <Column style={{ textAlign: 'center', minWidth: 42, maxWidth: 42 }}>
+                        <Avatar
+                          size={36}
+                          icon="user"
+                        />
+                      </Column>
 
-                    <Column>
-                      <div style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
-                        <div style={{ display: 'inline', fontWeight: 'bold' }}>
-                          {
-                            nickname || '[Untitled Peer]'
-                          }
+                      <Column>
+                        <div style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
+                          <div style={{ display: 'inline', fontWeight: 'bold' }}>
+                            {
+                              nickname || '[Untitled Peer]'
+                            }
+                          </div>
+
+                          <div style={{ display: 'inline', fontSize: '.8rem', fontStyle: 'italic', marginLeft: 4 }}>
+                            {
+                              `/ ${browserOnOs}`
+                            }
+                          </div>
                         </div>
-
-                        <div style={{ display: 'inline', fontSize: '.8rem', fontStyle: 'italic', marginLeft: 4 }}>
-                          {
-                            `/ ${browserOnOs}`
-                          }
+                        <div style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
+                          [ Last chat message ]
                         </div>
-                      </div>
-                      <div style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
-                        [ Last chat message ]
-                      </div>
-                    </Column>
+                      </Column>
 
-                    <Column style={{ maxWidth: 40 }}>
-                      abc
-                    </Column>
-                  </Row>
-                </div>
+                      <Column style={{ maxWidth: 40 }}>
+                        abc
+                      </Column>
+                    </Row>
+                  </div>
+                </Tooltip>
               </li>
             );
           })
