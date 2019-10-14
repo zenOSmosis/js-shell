cdd6a85fbfc10653ce84fed670061b7bc12755d0
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-10-14 12:10:38 -0600

    Added comments

--- frontend/src/components/Chat/SystemIcon/index.jsx
+++ frontend/src/components/Chat/SystemIcon/index.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { LaptopIcon, MobileIcon } from 'components/componentIcons';
 import PropTypes from 'prop-types';
 
+/**
+ * A dynamic icon, which represents either Laptop / Desktop, or Mobile,
+ * depending on the passed platformType.
+ * 
+ * @param {Object} props 
+ */
 const SystemIcon = (props) => {
   // TODO: Derive platformType from remotePeer
   const { platformType, ...propsRest } = props;
