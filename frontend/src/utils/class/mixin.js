f5a5bcbe62737476d32708a6b896387f9e2567c9
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-10-16 13:21:41 -0600

    Fixed linting / formatting issues with mixin.js

--- frontend/src/utils/class/mixin.js
+++ frontend/src/utils/class/mixin.js
@@ -15,13 +15,14 @@ const mixin = (parentInstance, childInstances = []) => {
       const l = Object.getOwnPropertyNames(obj)
         .concat(Object.getOwnPropertySymbols(obj).map(s => s.toString()))
         .sort()
-        .filter((p, i, arr) =>
+        .filter((p, i, arr) => // eslint-disable-line no-loop-func
           /* typeof obj[p] === 'function' && */  // only the methods
-          p !== 'constructor' &&           // not the constructor
-          (i == 0 || p !== arr[i - 1]) &&  // not overriding in this prototype
-          props.indexOf(p) === -1          // not overridden in a child
+          p !== 'constructor' &&            // not the constructor
+          (i === 0 || p !== arr[i - 1]) &&  // not overriding in this prototype
+          props.indexOf(p) === -1           // not overridden in a child
         );
-      props = props.concat(l)
+
+      props = props.concat(l);
     } while (
       (obj = Object.getPrototypeOf(obj)) &&   // walk-up the prototype chain
       Object.getPrototypeOf(obj)              // not the the Object prototype methods (hasOwnProperty, etc...)
