9de52d80cded0b4d6221e95542b87cf759701540
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-11-03 12:56:24 -0500

    Prototyping initial language selection

--- frontend/src/apps/SourceCodeApp/subComponents/AppFooter/AppFooter.jsx
+++ frontend/src/apps/SourceCodeApp/subComponents/AppFooter/AppFooter.jsx
@@ -1,103 +1,124 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LinkedStateRenderer from 'components/LinkedStateRenderer';
 import { ACTIVE_APP_FILE, CURSOR_POSITION, LANGUAGES } from '../../state/UniqueSourceCodeAppLinkedState';
 import { Row, Column } from 'components/Layout';
 import styles from './AppFooter.module.css';
 
-const AppFooter = (props) => {
-  const { editorLinkedState } = props;
+class AppFooter extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <LinkedStateRenderer
-      linkedState={editorLinkedState}
-      onUpdate={(updatedState) => {
-        let filteredState = {};
+  _handleEditorLanguageChange(language) {
+    // TODO: Implement
+    console.debug({
+      _handleEditorLanguageChange: language
+    });
+  }
 
-        if (updatedState[LANGUAGES]) {
-          filteredState.languages = updatedState[LANGUAGES];
-        }
+  render() {
+    const { editorLinkedState } = this.props;
 
-        let cursorPosition = null;
+    return (
+      <LinkedStateRenderer
+        linkedState={editorLinkedState}
+        onUpdate={(updatedState) => {
+          let filteredState = {};
 
-        if (updatedState[ACTIVE_APP_FILE]) {
-          const { language, meta: activeAppFileMeta } = updatedState[ACTIVE_APP_FILE];
-
-          filteredState.language = language;
-
-          // Obtain cursor position from activeAppFile meta property
-          const { [CURSOR_POSITION]: metaCursorPosition } = activeAppFileMeta;
-          if (metaCursorPosition) {
-            cursorPosition = metaCursorPosition;
+          if (updatedState[LANGUAGES]) {
+            filteredState.languages = updatedState[LANGUAGES];
           }
-        } else if (updatedState[CURSOR_POSITION]) {
-          // Obtain cursor position from updated editorLinkedState
-          cursorPosition = updatedState[CURSOR_POSITION];
-        }
 
-        if (cursorPosition) {
-          const { positionColumn, positionLineNumber } = cursorPosition;
+          let cursorPosition = null;
 
-          if (positionColumn !== undefined &&
-            positionLineNumber !== undefined) {
-            filteredState = {
-              ...filteredState, ...{
-                positionColumn,
-                positionLineNumber
-              }
-            };
-          }
-        }
+          if (updatedState[ACTIVE_APP_FILE]) {
+            const { language, meta: activeAppFileMeta } = updatedState[ACTIVE_APP_FILE];
 
-        return filteredState;
-      }}
-      render={renderProps => {
-        const { positionLineNumber, positionColumn, language, languages: propsLanguages } = renderProps;
-        const languages = propsLanguages ? propsLanguages : [];
+            filteredState.language = language;
 
-        return (
-          <Row className={styles['app-footer']}>
-            <Column>
-              Ln {positionLineNumber}, Col {positionColumn}
-            </Column>
-
-            <Column>
-              [ Spaces ]
-            </Column>
-
-            {
-              /*
-              <Column>
-                [ EOL Sequence ]
-              </Column>
-              */
+            // Obtain cursor position from activeAppFile meta property
+            const { [CURSOR_POSITION]: metaCursorPosition } = activeAppFileMeta;
+            if (metaCursorPosition) {
+              cursorPosition = metaCursorPosition;
             }
+          } else if (updatedState[CURSOR_POSITION]) {
+            // Obtain cursor position from updated editorLinkedState
+            cursorPosition = updatedState[CURSOR_POSITION];
+          }
 
-            <Column>
-              [ Encoding ]
-            </Column>
+          if (cursorPosition) {
+            const { positionColumn, positionLineNumber } = cursorPosition;
 
-            <Column>
-              {language}
-              <select>
-                {
-                  languages.map((_language, idx) => {
-                    return (
-                      <option
-                        key={idx}
-                        value={_language}
-                      >
-                        {_language}
-                      </option>
-                    );
-                  })
+            if (positionColumn !== undefined &&
+              positionLineNumber !== undefined) {
+              filteredState = {
+                ...filteredState, ...{
+                  positionColumn,
+                  positionLineNumber
                 }
-              </select>
-            </Column>
-          </Row>
-        );
-      }}
-    />
-  )
-};
+              };
+            }
+          }
+
+          return filteredState;
+        }}
+        render={renderProps => {
+          const {
+            positionLineNumber,
+            positionColumn,
+            language,
+            languages: propsLanguages
+          } = renderProps;
+          const languages = propsLanguages ? propsLanguages : [];
+
+          return (
+            <Row className={styles['app-footer']}>
+              <Column>
+                Ln {positionLineNumber}, Col {positionColumn}
+              </Column>
+
+              <Column>
+                [ Spaces ]
+              </Column>
+
+              {
+                /*
+                <Column>
+                  [ EOL Sequence ]
+                </Column>
+                */
+              }
+
+              <Column>
+                [ Encoding ]
+              </Column>
+
+              <Column>
+                {language}
+                <select
+                  // TODO: Handle editor language selection
+                  onChange={evt => this._handleEditorLanguageChange(evt.target.value)}
+                >
+                  {
+                    languages.map((_language, idx) => {
+                      return (
+                        <option
+                          key={idx}
+                          value={_language}
+                        >
+                          {_language}
+                        </option>
+                      );
+                    })
+                  }
+                </select>
+              </Column>
+            </Row>
+          );
+        }}
+      />
+    );
+  }
+}
 
 export default AppFooter;
